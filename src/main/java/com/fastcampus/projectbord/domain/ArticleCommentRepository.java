95e3357dabb49707070fd5cc6387fa1ed9bf24de
hgu5141 <93611963+hgu5141@users.noreply.github.com>
2024-04-28 17:46:22 +0900

    #1 - data rest 설정
    게시글, 댓글 JSON API 자동으로 RESULT되게 설정

--- src/main/java/com/fastcampus/projectbord/domain/ArticleCommentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.fastcampus.projectbord.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
-}
