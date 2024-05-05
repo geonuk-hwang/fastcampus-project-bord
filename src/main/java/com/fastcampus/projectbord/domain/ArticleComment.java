c9e695b0f70a97225153136fa709bff395fdff02
hgu5141 <93611963+hgu5141@users.noreply.github.com>
2024-05-05 18:31:59 +0900

    # 10 - AuditingFeilds 추출

--- src/main/java/com/fastcampus/projectbord/domain/ArticleComment.java
+++ src/main/java/com/fastcampus/projectbord/domain/ArticleComment.java
@@ -22,7 +22,7 @@ import java.util.Objects;
 })
 @EntityListeners(AuditingEntityListener.class)
 @Entity
-public class ArticleComment {
+public class ArticleComment extends AuditingFields {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -35,22 +35,6 @@ public class ArticleComment {
     @Column(nullable = false, length = 500)
     private String content;
 
-
-    @CreatedDate
-    @Column(nullable = false)
-    private LocalDateTime createdAt;
-
-    @CreatedBy
-    @Column(nullable = false, length = 100)
-    private String createdBy;
-
-    @LastModifiedDate
-    @Column(nullable = false)
-    private LocalDateTime modifiedAt;
-
-    @LastModifiedBy
-    @Column(nullable = false, length = 100)
-    private String modifiedBy;
 
     protected ArticleComment() {}
 
